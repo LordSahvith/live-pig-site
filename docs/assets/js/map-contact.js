988580838624652e2f0f3e2b93f75099b4dbd679
Caleb <bulletproofchild@gmail.com>
2018-03-14 13:57:00 -0600

    first commit.

---
+++ docs/assets/js/map-contact.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    initHeroMap();
+});
+
+function initHeroMap() {
+    var uluru = {lat: 40.7609211, lng: -111.8898534};
+    var map = new google.maps.Map(document.getElementById('map-contact'), {
+      zoom: 18,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
+
+//<script async defer
+//src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwlP7C-DXRvVnmnFlFoA1vQx_ULK74L5A&callback=initHeroMap">
+//</script>
